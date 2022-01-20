f3279f58f3a19d4ad2bfcc6acf1df183667a875f
Halis Yücel <isaycel2001@gmail.com>
2022-01-20 22:55:15 +0300

    roles were distributed to the players

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { EventEmitter } from 'events';
 import texts from './texts.js';
 
-// TODO: user disconnect emiti eklenecek
+// TODO: player disconnect emiti eklenecek
 
 class WerewolfGame {
     constructor({
@@ -11,14 +11,17 @@ class WerewolfGame {
         roles_config={},
     }) {
         this.eventEmitter = new EventEmitter();
-        this.users = {};
+        this.players = {};
         this.lang = lang;
         this.discussion_time = discussion_time;
         this.mix_roles = mix_roles;
         this.roles_config = roles_config;
+        this.lang_check();
     }
 
-    static userState = {
+    // static methods
+
+    static playerState = {
         alive: false,
         role: null,
         werewolf_bite: false,
@@ -33,38 +36,42 @@ class WerewolfGame {
 
     static roles = {
         villager: {
-            name: 'villager',
-            name_text: texts.villager[this.lang],
+            name: 'villager'
         },
         werewolf: {
-            name: 'werewolf',
-            name_text: texts.werewolf[this.lang],
+            name: 'werewolf'
         },
         oracle: {
-            name: 'oracle',
-            name_text: texts.oracle[this.lang],
+            name: 'oracle'
         },
         doctor: {
-            name: 'doctor',
-            name_text: texts.doctor[this.lang],
+            name: 'doctor'
         },
         wizard: {
-            name: 'wizard',
-            name_text: texts.wizard[this.lang],
+            name: 'wizard'
         },
         hunter: {
-            name: 'hunter',
-            name_text: texts.hunter[this.lang],
+            name: 'hunter'
         },
         gunslinger: {
-            name: 'gunslinger',
-            name_text: texts.gunslinger[this.lang],
+            name: 'gunslinger'
         },
     }
 
     static idGenerator() {
         return Math.random().toString(36).substring(2, 8);
     }
+
+    static shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    // event methods
 
     on() {
         return this.eventEmitter.on(...arguments);
@@ -75,21 +82,64 @@ class WerewolfGame {
         return this.eventEmitter.emit(...arguments);
     }
 
-    addUser({ 
+    // check methods
+    
+    lang_check() {
+        if (!texts.available_languages.includes(this.lang))
+            throw new Error(`"${this.lang}${texts.not_among_the_available_languages}`);
+    }
+
+    roles_config_check() {
+        if (!this.roles_config.hasOwnProperty('roles'))
+            return { error: true, errorMessage: texts.must_have_the_roles_property[this.lang]};
+
+        const roles = Object.keys(WerewolfGame.roles);
+        const config_roles = Object.keys(this.roles_config.roles);
+        let role_count = 0;
+        Object.values(config_roles).map(i => role_count += i);
+
+        if (role_count > Object.keys(this.players).length)
+            return { error: true, errorMessage: texts.more_roles_than_players[this.lang]};
+        
+        for (let i = 0; i < config_roles.length; i++) {
+            if (!roles.includes(config_roles[i]))
+                return { error: true, errorMessage: `${texts.mismatched_role_name[this.lang]}"${config_roles[i]}"!` };
+        }
+
+        if (!config_roles.includes('werewolf'))
+            return { error: true, errorMessage: texts.at_least_one_werewolf[this.lang]};
+
+        return { error: false, errorMessage: null };
+    }
+
+    start_check() {
+        if (Object.keys(this.players).length < 5) {
+            throw new Error(texts.not_enough_players[this.lang]);
+        }
+        if (!this.mix_roles) {
+            const {error, errorMessage} = this.roles_config_check();
+            if (error)
+                throw new Error(errorMessage);
+        }
+    }
+
+    // game methods
+
+    addPlayer({ 
         id=WerewolfGame.idGenerator(), 
         name,
         additional={},
     }) {
-        if (this.users[id])
-            throw new Error(texts.user_already_exists[this.lang]);
-        this.users[id] = { 
+        if (this.players[id])
+            throw new Error(texts.player_already_exists[this.lang]);
+        this.players[id] = { 
             id, 
             name,
             full_name: `${name} #${id}`, 
-            ...WerewolfGame.userState,
+            ...WerewolfGame.playerState,
             additional 
         };
-        this.eventEmitter.emit('user-added', this.users[id]);
+        this.eventEmitter.emit('player-added', this.players[id]);
     }
 
     distribute_roles() {
@@ -97,10 +147,10 @@ class WerewolfGame {
         if (this.mix_roles) {
             roles_list.push('werewolf');
             roles_list.push('villager');
-            while (roles_list.length < Object.keys(this.users).length) {
+            while (roles_list.length < Object.keys(this.players).length) {
                 const role = Object.keys(WerewolfGame.roles)[Math.floor(Math.random() * Object.keys(WerewolfGame.roles).length)];
                 if (role === 'werewolf') {
-                    if (roles_list.filter(i => i === 'werewolf').length * 5 < Object.keys(this.users).length)
+                    if (roles_list.filter(i => i === 'werewolf').length * 5 < Object.keys(this.players).length)
                         roles_list.push('werewolf');
                 }
                 else
@@ -108,35 +158,30 @@ class WerewolfGame {
             }
         }
         else {
-            
+            for (const role in this.roles_config.roles) {
+                for (let i = 0; i < this.roles_config.roles[role]; i++) {
+                    roles_list.push(role);
+                }
+            }
+            let role_count = 0;
+            Object.values(this.roles_config.roles).map(i => role_count += i);
+            console.log(role_count);
+            console.log(Object.keys(this.players).length);
+            if (Object.keys(this.players).length > role_count) {
+                for (let i = 0; i < (Object.keys(this.players).length - role_count); i++) {
+                    if (this.roles_config.randomly_assign_remaining_roles) {
+                        const werewolf_game_roles = Object.keys(WerewolfGame.roles).filter(i => i !== 'werewolf');
+                        const role = werewolf_game_roles[Math.floor(Math.random() * werewolf_game_roles.length)];
+                        roles_list.push(role);
+                    }
+                    else
+                        roles_list.push('villager');
+                }
+            }
         }
-        console.log(roles_list);
-    }
-
-    vote({ id, voteId }) {
-
-    }
-
-    roles_config_check() {
-        const roles = Object.keys(WerewolfGame.roles);
-        const config_roles = Object.keys(this.roles_config);
-        // role fit chechk
-        for (let i = 0; i < config_roles.length; i++) {
-            if (!roles.includes(config_roles[i]))
-                return { error: true, errorMessage: `${texts.mismatched_role_name[this.lang]}"${config_roles[i]}"!` };
-        }
-        // user count check
-        // TODO burada kaldın
-    }
-
-    start_check() {
-        if (this.users.length < 5) {
-            throw new Error(texts.not_enough_users[this.lang]);
-        }
-        if (!this.mix_roles) {
-            const {error, errorMessage} = this.roles_config_check();
-            if (error)
-                throw new Error(errorMessage);
+        WerewolfGame.shuffle(roles_list);
+        for (let i = 0; i < Object.keys(this.players).length; i++) {
+            this.players[Object.keys(this.players)[i]].role = WerewolfGame.roles[roles_list[i]];
         }
     }
 
@@ -148,17 +193,24 @@ class WerewolfGame {
 
 const game = new WerewolfGame({
     lang: 'tr',
-    mix_roles: false
+    mix_roles: false,
+    roles_config: {
+        roles: {
+            'werewolf': 2
+        },
+        randomly_assign_remaining_roles: true
+    }
 });
 
-game.on('user-added', (user) => {
-    console.log('yeni bir kullanıcı eklendi: ' + user.full_name);
+game.on('player-added', (player) => {
+    console.log('yeni bir oyuncu eklendi: ' + player.full_name);
 });
 
-game.addUser({ name: 'Halis' });
-game.addUser({ name: 'irgac' });
-game.addUser({ name: 'Tako' });
-game.addUser({ name: 'Yusuf' });
-game.addUser({ name: 'Dayi' });
+game.addPlayer({ name: 'player_1' });
+game.addPlayer({ name: 'player_2' });
+game.addPlayer({ name: 'player_3' });
+game.addPlayer({ name: 'player_4' });
+game.addPlayer({ name: 'player_5' });
+game.addPlayer({ name: 'player_6' });
 
 game.start();
--- texts.js
+++ texts.js
@@ -3,6 +3,7 @@ const texts = {
         'en',
         'tr',
     ],
+    not_among_the_available_languages: '" language is not among the available languages!',
     villager: {
         en: 'Villager',
         tr: 'Köylü'
@@ -31,17 +32,29 @@ const texts = {
         en: 'Gunslinger',
         tr: 'Silahşör'
     },
-    user_already_exists: {
-        en: 'User already exists',
-        tr: 'Kullanıcı zaten mevcut'
+    player_already_exists: {
+        en: 'Player already exists',
+        tr: 'Oyuncu zaten mevcut'
     },
-    not_enough_users: {
-        en: 'Not enough users, minimum is 4!',
-        tr: 'Kullanıcı sayısı yeterli değil, en az 4 olmalı!'
+    not_enough_players: {
+        en: 'Not enough players, minimum is 5!',
+        tr: 'Oyuncu sayısı yeterli değil, en az 5 olmalı!'
     },
     mismatched_role_name: {
         en: 'Mismatched role name: ',
         tr: 'Uyuşmayan rol ismi: '
+    },
+    at_least_one_werewolf: {
+        en: 'You must add at least one werewolf to the game!',
+        tr: 'Oyuna en az bir kurtadam eklemelisiniz!'
+    },
+    more_roles_than_players: {
+        en: 'You have more roles than players!',
+        tr: 'Oyuncudan fazla role sahipsiniz!'
+    },
+    must_have_the_roles_property: {
+        en: 'The "roles_config" object must have the "roles" property!',
+        tr: '"roles_config" objesi "roles" özelliğine sahip olmalıdır!'
     }
 }
 
